ec29e7aca96c40c6a3e5953de4b1a8b1cf36a2b5
tangbofan <1427866098@qq.com>
2020-11-10 17:54:45 +0800

    1

---
+++ build/precache-manifest.16b2f26c519afbfd84e9900afaad811f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "622053119e13fc9321ea29d3b0e5f156",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b828532b869c79d1654e",
+    "url": "./static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "12874ded42a46f07bcbd",
+    "url": "./static/css/main.20a12222.chunk.css"
+  },
+  {
+    "revision": "b828532b869c79d1654e",
+    "url": "./static/js/2.19c7a57d.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "./static/js/2.19c7a57d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12874ded42a46f07bcbd",
+    "url": "./static/js/main.9be07670.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  }
+]);
